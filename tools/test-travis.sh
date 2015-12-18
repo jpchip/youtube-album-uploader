f3c1aa6180417a24deb23d16b534b36fa8ade604
Jared Chapiewsky <chapiewsky@earthlinginteractive.com>
2015-12-18 14:36:39 -0600

    update test script to actually run tests

--- tools/test-travis.sh
+++ tools/test-travis.sh
@@ -18,4 +18,42 @@ export PATH=$(pwd)/bin:$PATH
 export ALT_FFMPEG_PATH=$(pwd)/ffmpeg
 export ALT_FFPROBE_PATH=$(pwd)/ffprobe
 
+if [ -z "$(which flvtool2)" ]; then
+	export FLVTOOL2_PRESENT=no
+else
+	export FLVTOOL2_PRESENT=yes
+fi
+
 echo travis_fold:end:Dependencies
+
+# Install nvm if needed
+
+echo travis_fold:start:nvm
+
+if [ ! -d ~/.nvm ]; then
+	wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.6.1/install.sh | sh
+fi
+source ~/.nvm/nvm.sh
+nvm install $NODE_VERSION
+
+echo travis_fold:end:nvm
+
+# Print versions
+
+echo travis_fold:start:Versions
+
+echo "node version: $(node --version)"
+echo "npm version: $(npm --version)"
+echo "ffmpeg version: $(ffmpeg -version)"
+
+echo travis_fold:end:Versions
+
+# Install dependencies
+echo travis_fold:start:npm-install
+
+npm install
+
+echo travis_fold:end:npm-install
+
+# Run tests
+npm test
